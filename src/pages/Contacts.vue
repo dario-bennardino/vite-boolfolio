59dab95c374cd8fac484b82eaaac90e45583f1da
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-06-05 17:41:16 +0200

    [add] controlli form

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -11,9 +11,15 @@ import Loader from '../components/partials/Loader.vue';
         data(){
             return{
                 name: 'dario',
-                email: 'dario@.it',
+                email: 'dario.bennardino@admin.it',
                 message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi qui sint, earum esse necessitatibus itaque distinctio consectetur quae ad ullam commodi architecto fuga doloremque magnam.',
                 sending: false,
+                sent: false,
+                errors:{
+                    name: [],
+                    email: [],
+                    message: []
+                }
             }
         },
         methods:{
@@ -23,13 +29,26 @@ import Loader from '../components/partials/Loader.vue';
                 const data = {
                     name: this.name,
                     email: this.email,
-                    message: this.message
+                    message: this.message,
+                    
                 }
 
                 axios.post(store.apiUrl + 'send-email', data)
                     .then(result => {
                         this.sending = false;
                         console.log(result.data);
+                        this.sent = result.data.success;
+                        if(!result.data.success){
+                            this.errors = result.data.errors;    
+                        }else{
+                            this.errors = {
+                                name: [],
+                                email: [],
+                                message: []
+                            }
+                        }
+
+                        
                     })
                     .catch(err =>{
                         this.sending = false;
@@ -45,36 +64,45 @@ import Loader from '../components/partials/Loader.vue';
 <template>
     <div class="my-4">
         <h1>Contatti</h1>
-        <form v-if="!sending" @submit.prevent="sendEmail">
-            <div class="mb-3 w-50">
-                <label for="name" class="form-label">Nome</label>
-                <input v-model="name" type="text" id="name" class="form-control"  >
-                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
-            </div>
 
-            <div class="mb-3 w-50">
-                <label for="email" class="form-label">Email</label>
-                <input v-model="email" type="text" class="form-control" name="email" id="email">
-                <p class="error"></p>
-                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
-            </div>
+        <div v-if="!sending">
+            <form v-if="!sent" @submit.prevent="sendEmail">
+                <div class="mb-3 w-50">
+                    <label for="name" class="form-label">Nome</label>
+                    <input v-model="name" type="text" id="name" class="form-control"  >
+                    <p class="error text-danger">{{ errors.name?.toString() }}</p>
+                    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                </div>
 
-            <!-- <div class="mb-3">
-                <label for="message" class="form-label">Messaggio</label>
-                <textarea v-model="message" name="message" id="message"></textarea>
-            </div> -->
+                <div class="mb-3 w-50">
+                    <label for="email" class="form-label">Email</label>
+                    <input v-model="email" type="text" class="form-control" name="email" id="email">
+                    <p class="error text-danger is-invalid">{{ errors.email?.toString() }}</p>
+                    
+                    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                </div>
 
-            <div class="mb-3 w-50">
-                <label for="message" class="form-label">Messaggio</label>
-                <textarea v-model="message" name="message" id="message" class="form-control"></textarea>
-            </div>
+                <!-- <div class="mb-3">
+                    <label for="message" class="form-label">Messaggio</label>
+                    <textarea v-model="message" name="message" id="message"></textarea>
+                </div> -->
 
-            <!-- <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div> -->
-            <button type="submit" class="btn btn-primary">Invia</button>
-        </form>
+                <div class="mb-3 w-50">
+                    <label for="message" class="form-label">Messaggio</label>
+                    <textarea v-model="message" name="message" id="message" class="form-control"></textarea>
+                    <p class="error text-danger">{{ errors.message?.toString() }}</p>
+                </div>
+
+                <!-- <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div> -->
+                <button type="submit" class="btn btn-primary">Invia</button>
+            </form>
+            <h3 v-else>Email inviata correttamente</h3>
+        </div>
+
+        
         <Loader v-else />
 
         <!-- <form>
