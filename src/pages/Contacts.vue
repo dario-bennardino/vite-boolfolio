ac8ad486a97b0821659f1aaa54d39245a6e018fb
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-06-05 14:51:06 +0200

    [add] form

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,12 +1,57 @@
 <script>
     export default {
-        name: 'Contacts'
+        name: 'Contacts',
+        methods:{
+            sendMail(){
+                
+            }
+        }
     }
 </script>
 
 <template>
-    <div>
-        <h1>contacts</h1>
+    <div class="my-4">
+        <h1>Contatti</h1>
+        <form>
+            <div class="mb-3 w-50">
+                <label for="name" class="form-label">Nome</label>
+                <input v-model="name" type="text" id="name" class="form-control"  >
+                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+            </div>
+
+            <div class="mb-3 w-50">
+                <label for="email" class="form-label">Email</label>
+                <input v-model="email" type="text" class="form-control" name="email" id="email">
+                <p class="error"></p>
+                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+            </div>
+
+            <!-- <div class="mb-3">
+                <label for="message" class="form-label">Messaggio</label>
+                <textarea v-model="message" name="message" id="message"></textarea>
+            </div> -->
+
+            <div class="mb-3 w-50">
+                <label for="message" class="form-label">Messaggio</label>
+                <textarea v-model="message" name="message" id="message" class="form-control"></textarea>
+            </div>
+
+            <!-- <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div> -->
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+        <!-- <form>
+            <div>
+                <div>
+                    <label for="name">Nome</label>
+                    <input v-model="name" type="text" name="name" >
+                    <p class></p>
+                </div>
+            </div>
+        </form> -->
     </div>
 </template>
 
